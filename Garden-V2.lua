3856e507a61006e3ddfa8569a1ef01a23f2dd5a3
No-lagHub <sumedangfy@gmail.com>
2025-05-29 18:12:56 +0700

    Update Garden-V2.lua

--- Garden-V2.lua
+++ Garden-V2.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/araEo83Z/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/xgjFxryu/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/No-lagHub/Garden/refs/heads/main/Garden-V2.lua"))()
